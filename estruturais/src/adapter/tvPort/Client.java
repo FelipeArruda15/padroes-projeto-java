aeb1f0254c84c24f837a4547584c5fa364ad5732
FelipeArruda15 <souzafelipesouza5@gmail.com>
2023-06-26 20:09:53 -0300

    Aplicando padrão adapter(adaptador de objetos)

--- estruturais/src/adapter/tvPort/Client.java
+++ estruturais/src/adapter/tvPort/Client.java
@@ -1,20 +1,16 @@
 package adapter.tvPort;
 
+import adapter.tvPort.adapters.HDMIToVGAAdapter;
 import adapter.tvPort.model.Computer;
 import adapter.tvPort.model.OldMonitor;
-import adapter.tvPort.model.TV;
 
 public class Client {
 
     public static void main(String[] args) {
-        TV tv = new TV();
         OldMonitor monitor = new OldMonitor();
         Computer computer = new Computer();
-        // Consigo conectar a TV na porta do computador pois são de tipos
-        // compatives(HDMI)
-        computer.connectPort(tv);
-        // Tipos incompativeis
-        // computer.connectPort(monitor);
+        computer.connectPort(new HDMIToVGAAdapter(monitor));
+        computer.sendImageAndSound("Cr7 bike x JUve", "SIIIIIIIII");
     }
 
 }
