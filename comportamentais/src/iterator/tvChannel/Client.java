3fdd9a2088422b2929909e1acd67d7af10041bcb
FelipeArruda15 <souzafelipesouza5@gmail.com>
2023-08-28 23:52:57 -0300

    Iterator - Tv channel

---
+++ comportamentais/src/iterator/tvChannel/Client.java
@@ -0,0 +1,14 @@
+package iterator.tvChannel;
+
+public class Client {
+
+    public static void main(String[] args) {
+        TV tv = new TV();
+        tv.searchAvaiableChannels();
+
+        for (Integer channel : tv) {
+            System.out.println(String.format("Zapping at channel #%d", channel));
+        }
+    }
+
+}
